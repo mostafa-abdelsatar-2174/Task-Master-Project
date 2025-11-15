f2340237d07da84bb9fe22701dabe85c0be87816
Mostafa Mohammed Abdelsatar <mosasa1112@gmail.com>
2025-11-15 13:30:20 +0200

    Add new route for Task-Master-Project

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
             <Routes>
               {/* Public Routes */}
               <Route path="/" element={<Home />} />
+              <Route path="/Task-Master-Project" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} />
               <Route path="/login" element={<Login />} />
