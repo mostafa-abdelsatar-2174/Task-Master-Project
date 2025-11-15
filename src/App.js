3e8705de549a81d8efd7251f9cf374472fda3aab
Mostafa Mohammed Abdelsatar <mosasa1112@gmail.com>
2025-11-15 13:39:11 +0200

    Remove redundant route for Task-Master-Project

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ function App() {
             <Routes>
               {/* Public Routes */}
               <Route path="/" element={<Home />} />
-              <Route path="/Task-Master-Project" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} />
               <Route path="/login" element={<Login />} />
